a38661ec56ff0f2ec8feb743fc8be8a3d663082c
Samuel W <68044712+Samuel-KW@users.noreply.github.com>
2023-03-21 14:49:57 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const averageWPM = 250;
 
-function relativeTime(seconds) {
+const getRelativeTime = seconds => {
 
   if (seconds < 60)
-    return parseInt(seconds) + 's';
+    return parseInt(seconds) + ' sec';
   
   if (seconds < 3600) 
-    return parseInt(seconds / 60) + 'm';
+    return parseInt(seconds / 60) + ' min';
   
-  return parseInt(seconds / 3600) + 'h';
-}
+  return parseInt(seconds / 3600) + ' hour';
+};
 
+const getReadingTime = elem => {
+  const content = elem.textContent.split(' ');
+  const wpm = content.length / averageWPM;
+  
+  return getRelativeTime(wpm * 60) + ' read';
+};
 
