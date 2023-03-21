80472061d1be4c78928ccd37bb88bf54c5c51fa2
Samuel W <68044712+Samuel-KW@users.noreply.github.com>
2023-03-21 16:24:31 -0700

    Added a bit of documentation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const averageWPM = 250;
 
+/*
+ * Converts a time in seconds to a convenient human readable relative time
+ */
 const getRelativeTime = seconds => {
 
   if (seconds < 60)
@@ -11,6 +14,9 @@ const getRelativeTime = seconds => {
   return parseInt(seconds / 3600) + ' hour';
 };
 
+/*
+ * Estimates the time to read the content of an HTML element
+ */
 const getReadingTime = elem => {
   const content = elem.textContent.split(' ');
   const wpm = content.length / averageWPM;
